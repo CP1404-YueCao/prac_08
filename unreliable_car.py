731c2aa6608a5495d762eb085c6d3eedf8b725d3
Yue Cao <yue.cao@my.jcu.edu.au>
2022-01-10 22:09:32 +0800

    Add init function

--- unreliable_car.py
+++ unreliable_car.py
@@ -3,4 +3,10 @@ from car import Car
 
 class UnreliableCar(Car):
     """An unreliable version of cars"""
+
+
+def __init__(self, name, fuel, reliability):
+    """Initialise an UnreliableCar"""
+    super().__init__(name, fuel)
+    self.reliability = reliability
    
