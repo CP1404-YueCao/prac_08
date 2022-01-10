b8d2b0e3d1473fda9fa06b219ab89083ffa3822d
Yue Cao <yue.cao@my.jcu.edu.au>
2022-01-10 22:15:21 +0800

    Add drive function, and generates a random number that only drive the car if that number is less than the car's reliability

--- unreliable_car.py
+++ unreliable_car.py
@@ -1,12 +1,19 @@
 from car import Car
+from random import randint
 
 
 class UnreliableCar(Car):
     """An unreliable version of cars"""
 
+    def __init__(self, name, fuel, reliability):
+        """Initialise an UnreliableCar"""
+        super().__init__(name, fuel)
+        self.reliability = reliability
 
-def __init__(self, name, fuel, reliability):
-    """Initialise an UnreliableCar"""
-    super().__init__(name, fuel)
-    self.reliability = reliability
-   
+    def drive(self, distance):
+        """Drive the car if that number is less than the car's reliability"""
+        random_num = randint(0, 100)
+        if random_num >= self.reliability:
+            distance = 0
+        drive_distance = super().drive(distance)
+        return drive_distance
