1fc1803265fa71ebd934ee54b804291dcf91b03b
Yue Cao <yue.cao@my.jcu.edu.au>
2022-01-10 21:59:36 +0800

    Change the client code so that it doesn't pass in the value

--- taxi_test.py
+++ taxi_test.py
@@ -3,7 +3,7 @@ from taxi import Taxi
 
 def main():
     # TODO: Create a new taxi with name "Prius 1", 100 units of fuel and price of $1.23/km
-    taxi = Taxi("Prius 1", 100, 1.23)
+    taxi = Taxi("Prius 1", 100)
     # TODO: Drive the taxi 40 km
     taxi.drive(40) # odo = 40, fuel = 60, current_fare_distance = 40
     # TODO: Print the taxi's details and the current fare
