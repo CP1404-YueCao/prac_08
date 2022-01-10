92451fd023706fbc38e1faf6b2b7f354b5eb2862
Yue Cao <yue.cao@my.jcu.edu.au>
2022-01-10 22:47:31 +0800

    Add a __str__ method to return a string description of a SilverServiceTaxi

--- silver_service_taxi.py
+++ silver_service_taxi.py
@@ -14,3 +14,7 @@ class SilverServiceTaxi(Taxi):
     def get_fare(self):
         """Calculate the current fare"""
         return self.flagfall + super().get_fare()
+
+    def __str__(self):
+        """Return a string description of a SilverServiceTaxi"""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
